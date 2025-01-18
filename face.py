acf0d24bc7ca7aee32e5733ccaa1ac5c25bd03e1
Saki Sakira <sakisakira@gmail.com>
2025-01-18 22:24:34 +0900

    Started preparing to change face expression.

--- face.py
+++ face.py
@@ -2,19 +2,19 @@ import pyxel
 from world import *
 
 class Face:
+    Size = Vec2(64, 64)
+    Scale = 2
+    
     def __init__(self, image_index):
         self.image_index = image_index
         self.image = pyxel.images[image_index]
         self.image.load(0, 0, "images/faces.png")
-        self.width = 64
-        self.height = 64
-        self.scale = 2
 
     def show(self):
         margin = 5
-        x = world.screen_size.x - self.width - self.width / self.scale - margin
-        y = margin + self.height / self.scale
+        x = world.screen_size.x - self.Size.x - self.Size.x / self.Scale - margin
+        y = margin + self.Size.y / self.Scale
         pyxel.blt(x, y, self.image_index,
-                  0, 0, self.width, self.height,
-                  world.bg_index, 0, self.scale)
+                  0, 0, self.Size.x, self.Size.y,
+                  world.bg_index, 0, self.Scale)
                   
