892d5f3c9240cac0f08ad53fe80171d133934d18
Saki Sakira <sakisakira@gmail.com>
2025-01-19 12:39:23 +0900

    Updated stages.py as a preparation of multiple stages.

--- stages.py
+++ stages.py
@@ -3,14 +3,24 @@ from world import *
 from physics import *
 
 class Stage:
-    def __init__(self, ground):
-        self.ground = ground
+    def __init__(self, xy_array):
+        self.ground = self.gen_ground(xy_array)
 
-ground = Ground(
-    [Vec2(-3.0, 0.0),
-     Vec2(5.0, 1.0),
-     Vec2(20.0, 0.0),
-     Vec2(100.0, 1.0)])
+    def gen_ground(self, xy_array):
+        l = len(xy_array)
+        if l % 2 != 0: raise
+        vec2_array = []
+        for index in range(l // 2):
+            x = xy_array[index * 2 + 0]
+            y = xy_array[index * 2 + 1]
+            vec2_array.append(Vec2(x, y))
+        return Ground(vec2_array)
 
 stages = []
-stages.append(Stage(ground))
+
+# stage 1
+xys = [ -3.0, 0.0,
+        5.0, 1.0,
+        20.0, 0.0,
+        100.0, 1.0]
+stages.append(Stage(xys))
