8a04b81a7f45e32e3e7deedef3a07c02fd2002fe
Saki Sakira <sakisakira@gmail.com>
2025-08-04 12:00:22 +0900

    Enabled to save high score on web.

--- motorcycle/savedata.py
+++ motorcycle/savedata.py
@@ -3,32 +3,50 @@ import json
 import pyxel
 from world import *
 
+# reference: https://note.com/frenchbread/n/n9a46ada3850e
+IsWeb = True
+try:
+    from js import window
+except:
+    IsWeb = False
+
 class Savedata:
-    Directory = False
     TagTotal = "total"
     
     def __init__(self):
         self.filepath = False
+        self.storage_item = False
         self.data = False
 
     def load(self):
-        if not Savedata.Directory:
-            Savedata.Directory = pyxel.user_data_dir("Sasakiki", "Motorcycle Yumia")
-        if not self.filepath:
-            self.filepath = Savedata.Directory + "savedata_" + g_world.Version
-            print(self.filepath)
-        if not os.path.isfile(self.filepath):
-            self.data = {}
-            return
         try:
-            with open(self.filepath, 'r') as f:
-                self.data = json.load(f)
-        except json.JSONDecodeError:
+            if IsWeb:
+                self.storage_item = "bumping_rinder_savedata_" + g.world.Version
+                data_str = window.localStorage.getItem(self.storage_item)
+            else:
+                dir = pyxel.user_data_dir("Sasakiki", "Bumping Rider")
+                if not self.filepath:
+                    self.filepath = dir + "savedata_" + g_world.Version
+                    print(self.filepath)
+                    if not os.path.isfile(self.filepath):
+                        self.data = {}
+                        return
+                with open(self.filepath, 'r') as f:
+                    data_str = f.read()
+            self.data = json.load(data_str)
+        except Exception:
             self.data = {}
 
     def save(self):
-        with open(self.filepath, 'w') as f:
-            json.dump(self.data, f)
+        try:
+            data_str = json.dumps(self.data)
+            if IsWeb:
+                window.localStorage.setItem(self.storage_item, data_str)
+            else:
+                with open(self.filepath, 'w') as f:
+                    f.write(data_str)
+        except Exception:
+            pass
 
     def set_time(self, tag, time):
         if not self.data:
